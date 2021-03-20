2e07e35fe33d4ca15a8fe1c1ae6963623ac1361c
Paul Aswa <paul@opencraft.com>
2021-03-20 16:02:05 +0300

    [BB-3867] Update ThemeFooterSidebar snapshot

--- frontend/src/newConsole/components/newFooter/ThemeFooter.spec.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { setupComponentForTesting } from "utils/testing";
-import { ThemeFooterSideBar } from './ThemeFooterSideBar';
-
-it('renders without crashing', () => {
-    const tree = setupComponentForTesting(<ThemeFooterSideBar />).toJSON();
-    expect(tree).toMatchSnapshot();
-});
